317151c8164b624562cf586055632f7704dc09eb
Sam Starling <42478+samstarling@users.noreply.github.com>
2025-11-29 14:30:43 +0000

    Introduce options pattern

--- block_work_object.go
+++ block_work_object.go
@@ -4,6 +4,18 @@ type BlockWorkObject struct {
 	BlockID string `json:"block_id,omitempty"`
 }
 
+type WorkObjectBlockOption func(*BlockWorkObject)
+
+func NewBlockWorkObject(blockID string, options ...WorkObjectBlockOption) *BlockWorkObject {
+	block := &BlockWorkObject{BlockID: blockID}
+
+	for _, option := range options {
+		option(block)
+	}
+
+	return block
+}
+
 func (b BlockWorkObject) BlockType() MessageBlockType {
 	return MBTWorkObject
 }
