9e3e0e368855a5825f5179b24d568054892d165b
Sam Starling <42478+samstarling@users.noreply.github.com>
2025-11-29 14:38:10 +0000

    Nicer variable naming

--- work_object.go
+++ work_object.go
@@ -6,11 +6,11 @@ type WorkObject struct {
 type WorkObjectOption func(*WorkObject)
 
 func NewBlockWorkObject(blockID string, options ...WorkObjectOption) *WorkObject {
-	block := &WorkObject{}
+	o := &WorkObject{}
 
 	for _, option := range options {
-		option(block)
+		option(o)
 	}
 
-	return block
+	return o
 }
